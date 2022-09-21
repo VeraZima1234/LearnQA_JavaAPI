828e4a17843d2059340abba43121bfdd80a892b8
VZagrubskaya <vera.zagrubskaya@firstlinesoftware.com>
2022-09-21 17:31:10 +0300

    add testUserAgent

--- src/test/java/RestAssuredClass.java
+++ src/test/java/RestAssuredClass.java
@@ -1,12 +1,16 @@
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.sql.Time;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.SoftAssertions;
 
 public class RestAssuredClass {
 
@@ -218,5 +222,63 @@ public class RestAssuredClass {
 
         assertEquals("Some secret value",response.getHeader("x-secret-homework-header"), "Header value is wrong");
 
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings={
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
+    public void testUserAgent(String userAgent)
+    {
+        Response response = RestAssured
+                .given()
+                .header("User-Agent",userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+
+        SoftAssertions assertions=userAgentValidation(response,userAgent);
+        assertions.assertAll();
+
+    }
+
+    @Step
+    public SoftAssertions userAgentValidation(Response response,String userAgent) {
+        SoftAssertions assertions = new SoftAssertions();
+        if (userAgent.equals("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30")) {
+            assertions=userAgentParametersValidation(response,"Mobile","No","Android");
+
+        } else if (userAgent.equals("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1")) {
+            assertions=userAgentParametersValidation(response,"Mobile","Chrome","iOS");
+
+        } else if (userAgent.equals("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")) {
+            assertions=userAgentParametersValidation(response,"Googlebot","Unknown","Unknown");
+
+        } else if (userAgent.equals("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0")) {
+            assertions=userAgentParametersValidation(response,"Web","Chrome","No");
+
+        } else if (userAgent.equals("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")) {
+            assertions=userAgentParametersValidation(response,"Mobile","No","iPhone");
+
+        }
+        return assertions;
+    }
+
+    @Step
+    public SoftAssertions userAgentParametersValidation(Response response,String platform,String browser,String device) {
+        SoftAssertions assertions = new SoftAssertions();
+        assertions.assertThat(platform)
+                .overridingErrorMessage("Platform is wrong")
+                .isEqualTo(response.jsonPath().getString("platform"));
+        assertions.assertThat(browser)
+                .overridingErrorMessage("Browser is wrong")
+                .isEqualTo(response.jsonPath().getString("browser"));
+        assertions.assertThat(device)
+                .overridingErrorMessage("Device is wrong")
+                .isEqualTo(response.jsonPath().getString("device"));
+        return assertions;
     }
 }
