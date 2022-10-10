ce0541fe7c2d0176ebb7d12fb1cf06438d44560a
VZagrubskaya <vera.zagrubskaya@firstlinesoftware.com>
2022-10-10 12:43:00 +0300

    add user tests

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -3,6 +3,7 @@ package lib;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Assertions {
@@ -11,5 +12,37 @@ public class Assertions {
         Response.then().assertThat().body("$",hasKey(name));
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue,value,"JSON value is wrong");
+    }
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+        assertEquals(
+                expectedStatusCode,
+                Response.statusCode(),
+                "Response statusCode is not as expected"
+        );
+    }
+
+    public static void assertJsonHasField(Response Response, String expectedFieldName)
+    {
+        Response.then().assertThat().body("$", hasKey(expectedFieldName));
+    }
+
+    public static void assertJsonHasNotField(Response Response, String unexpectedFieldName){
+        Response.then().assertThat().body("$",not(hasKey(unexpectedFieldName)));
+    }
+
+    public static void assertJsonHasFields(Response Response, String[] expectedFieldNames)
+    {
+        for(String expectedFieldName:expectedFieldNames) {
+            Response.then().assertThat().body("$", hasKey(expectedFieldName));
+        }
     }
 }
---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends BaseTestCase {
+
+
+    @Test
+    public void testCreateUserWithExistingEmail(){
+        String email= "vinkotov@example.com";
+        Map<String,String> userData=new HashMap<>();
+        userData.put("email","vinkotov@example.com");
+        userData.put("password","123");
+        userData.put("username","learnqa");
+        userData.put("firstName","learnqa");
+        userData.put("lastName","learnqa");
+
+        Response responseCreateAuth= RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseTextEquals(responseCreateAuth,"Users with email '"+email+"' already exists");
+        Assertions.assertResponseCodeEquals(responseCreateAuth,400);
+    }
+
+    @Test
+    public void testCreateUserSuccessfully(){
+        String email= DataGenerator.getRandomEmail();
+        Map<String,String> userData=new HashMap<>();
+        userData.put("email",email);
+        userData.put("password","123");
+        userData.put("username","learnqa");
+        userData.put("firstName","learnqa");
+        userData.put("lastName","learnqa");
+
+        Response responseCreateAuth= RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth,200);
+        Assertions.assertJsonHasField(responseCreateAuth,"id");
+    }
+}
